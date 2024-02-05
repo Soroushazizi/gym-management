81b65707c16eaa8a1bc2635fdd123952893b4989
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-05 20:16:41 +0330

    Debug Member Controller & Routes

--- backend/routes/memberRoutes.js
+++ backend/routes/memberRoutes.js
@@ -3,7 +3,6 @@ const memberController = require('../controllers/memberController');
 const {checkRole} = require("../Util");
 const router = express.Router();
 
-router.post('/login', memberController.login);
 router.post('/payment', checkRole('member'), memberController.payment);
 
 module.exports = router;
