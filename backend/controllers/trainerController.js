b00baea9869284a7e8fdd7690649918fa9e0bf92
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-05 20:16:51 +0330

    Debug Trainer Controller & Routes

--- backend/controllers/trainerController.js
+++ backend/controllers/trainerController.js
@@ -1,19 +1,8 @@
-const User = require('../models/User');
 const Attendance = require('../models/Attendance');
+const {validate, checkRole} = require("../Util");
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const secretKey = 'your_secret_key'; // Replace with your secret key
-
-exports.login = async (req, res) => {
-    try {
-        const trainer = await User.findOne({ username: req.body.username, role: 'trainer' });
-        if (!trainer) return res.status(404).send({ error: 'Trainer not found' });
-
-        const token = jwt.sign({ id: trainer._id }, secretKey, { expiresIn: '6h' });
-        res.status(200).send({ token });
-    } catch (error) {
-        res.status(500).send({ error: 'Server error' });
-    }
-};
 
 exports.attendance = async (req, res) => {
     try {
