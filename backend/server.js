59978221899ab58f2bfc348fe2a72dec60d33f5a
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-05 20:17:14 +0330

    Debug Server & Add Docker

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const memberRoutes = require('./routes/memberRoutes');
 const trainerRoutes = require('./routes/trainerRoutes');
 
+app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/member', memberRoutes);
 app.use('/api/trainer', trainerRoutes);
 
 // Connect to MongoDB
-mongoose.connect('YOUR_DATABASE_URL', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/newdb', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const PORT = 5000;
 app.listen(PORT, () => {
