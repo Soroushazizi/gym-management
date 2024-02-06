7fb3591fe4e3bb3684e5bc9b3916c2841c30f8de
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-06 07:58:51 +0330

    Debug Admin Member Edit And Trainer Edit

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,10 +5,9 @@ const router = express.Router();
 
 router.post('/member', checkRole('admin'), adminController.addMember);
 router.delete('/member/:id', checkRole('admin'), adminController.deleteMember);
-router.get('/trainer', checkRole('admin'), adminController.showTrainers);
-router.post('/trainer', [validate('addTrainer'), checkRole('admin')], adminController.addTrainer);
-router.delete('/trainer/:id', [validate('deleteTrainer'), checkRole('admin')], adminController.deleteTrainer);
-router.patch('/member/:id', [validate('modifyMemberData'), checkRole('admin')], adminController.modifyMemberData);
-router.patch('/trainer/:id', [validate('modifyTrainerShifts'), checkRole('admin')], adminController.modifyTrainerShifts);
+router.post('/trainer',  checkRole('admin'), adminController.addTrainer);
+router.delete('/trainer/:id', checkRole('admin'), adminController.deleteTrainer);
+router.put('/member/:id',  checkRole('admin'), adminController.modifyMemberData);
+router.put('/trainer/:id', checkRole('admin'), adminController.modifyTrainerShifts);
 
 module.exports = router;
