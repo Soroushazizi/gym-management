1048f344e5178e5183420e71bf3dbd5a34cbb83a
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-06 07:59:34 +0330

    Debug Attendance Page

---
+++ assets/pages/Attendance/attendance.js
@@ -0,0 +1,24 @@
+let params = new URLSearchParams(window.location.search);
+let id = params.get('id');
+
+
+async function attend(value) {
+    const data = {attended : value}
+    const response = await fetch('http://localhost:5000/api/trainer/attendance/' + id, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': localStorage.getItem('token')
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+        alert('Done')
+        window.location.reload()
+    } else if (response.status === 404) {
+        alert('User not registered');
+    } else {
+        console.error(`Error: ${response.status}`);
+    }
+}
