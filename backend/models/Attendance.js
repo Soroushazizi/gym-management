8df5478cf7553c9940270ad275a5744039713d1c
Alireza <alireza.dolatabadi.jc@gmail.com>
2024-02-06 07:59:10 +0330

    Debug Attendance Model

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const AttendanceSchema = new mongoose.Schema({
     member: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Membership',
         required: true
     },
     trainer: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Trainer',
         required: true
     },
     date: {
